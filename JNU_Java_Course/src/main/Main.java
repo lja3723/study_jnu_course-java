c278c44ac90ac66522b21b7b47e17f64625feab3
lja3723 <lja3723@gmail.com>
2023-03-06 11:57:55 +0900

    2nd

--- JNU_Java_Course/src/main/Main.java
+++ JNU_Java_Course/src/main/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		for(int i = 0; i < 100; i++) {
+			System.out.print("*");
+			if ((i + 1) % 10 == 0)
+				System.out.print("\n");
+		}
+		
 	}
 
 }
