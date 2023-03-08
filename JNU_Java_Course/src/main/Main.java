7b9407bf207e26ecf6e81a641cde72e777e52f19
lja3723 <lja3723@gmail.com>
2023-03-08 11:11:20 +0900

    3rd

--- JNU_Java_Course/src/main/Main.java
+++ JNU_Java_Course/src/main/Main.java
@@ -1,16 +1,14 @@
 package main;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-		for(int i = 0; i < 100; i++) {
-			System.out.print("*");
-			if ((i + 1) % 10 == 0)
-				System.out.print("\n");
+		int called = 500;
+		int bitts = called-50;
+		int answer = 0;
+		for (int i = 5; i < 2000; i++) {
+			answer += (called + bitts) * (double)i/2000;
 		}
+		System.out.println(answer);
 		
 	}
-
 }
